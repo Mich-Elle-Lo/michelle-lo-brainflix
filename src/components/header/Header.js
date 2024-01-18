bcd9e89d8d2663a02596bc042bef48d14d82c3cb
Michelle Lo <michellelo@live.ca>
2024-01-17 23:03:33 -0500

    add partials, header styles

---
+++ src/components/header/Header.js
@@ -0,0 +1,45 @@
+// import React from "react";
+import "./Header.scss";
+// import logo from "../../assets/Logo/BrainFlix-logo.svg";
+import searchlogo from "../../assets/Icons/search.svg";
+import uploadlogo from "../../assets/Icons/upload.svg";
+
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="header__logobox">
+          <div className="header__logo"> </div>
+        </div>
+        <div className="header__flex">
+          <div className="header__mobileflex">
+            <div className="header__search">
+              <img
+                className="header__searchlogo"
+                src={searchlogo}
+                alt="search logo"
+              />
+              <input
+                type="text"
+                placeholder="Search"
+                id="searchBar"
+                className="header__input"
+              />
+            </div>
+            <div className="header__profile header__profile--mobile"></div>
+          </div>
+          <button className="header__button">
+            <img
+              className="header__uploadlogo"
+              src={uploadlogo}
+              alt="Upload logo"
+            />
+            UPLOAD
+            <div className="header__buttonempty"></div>
+          </button>
+          <div className="header__profile header__profile--tablet"></div>
+        </div>
+      </div>
+    </>
+  );
+}
